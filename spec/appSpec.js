455590e9c7e0fce434763fcf9fb080d9604386e8
Sébastien de Beauffort <sebastien.debeauffort@vinci.be>
2023-10-05 17:26:26 +0200

    add: manage the case 1

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -1,14 +1,6 @@
 import { Main } from '../src/app.js';
 
 describe("A suite of test for the demo function of the main class", function() {
-    it("demo function should return 1", function() {
-        let main = new Main();
-
-        let result = main.demo();
-
-        expect(result).toBe(1);
-    });
-
     it("fizzBuzz function return 1 when given 1", function() {
         let main = new Main();
 
