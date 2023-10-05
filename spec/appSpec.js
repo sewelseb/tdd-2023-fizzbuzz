9c8bc5f79a57ec88592d35cc76e57393834639ea
Sébastien de Beauffort <sebastien.debeauffort@vinci.be>
2023-10-05 17:53:24 +0200

    add: fizzbuzz returns buzz when given 5

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -12,11 +12,21 @@ describe("A suite of test for the demo function of the main class", function() {
         let result = main.fizzByzz(2);
 
         expect(result).toBe(2);
-    })
+    });
     it("fizzbuzz function should return fizz when given 3", function() {
         let result = main.fizzByzz(3);
 
         expect(result).toBe("fizz");
-    })
+    });
+    it("fizzBuzz function should return 4 when given 4", function() {
+        let result = main.fizzByzz(4);
+
+        expect(result).toBe(4);
+    });
+    it("fizzBuzz function should return buzz when given 5", function() {
+        let result = main.fizzByzz(5);
+
+        expect(result).toBe("buzz");
+    });
 });
 
--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 export class Main {
     fizzByzz(number) {
         if(number === 3) return "fizz";
+        if(number === 5) return "buzz";
 
         return number;
     }
