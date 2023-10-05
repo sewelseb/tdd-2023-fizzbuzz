6e0e82cc69535a660c38fbe14fc68a65fde5621c
Sébastien de Beauffort <sebastien.debeauffort@vinci.be>
2023-10-05 17:24:53 +0200

    add: manage the case 1

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -1,10 +1,18 @@
 import { Main } from '../src/app.js';
 
-describe("A suite", function() {
+describe("A suite of test for the demo function of the main class", function() {
     it("demo function should return 1", function() {
         let main = new Main();
 
         let result = main.demo();
+
+        expect(result).toBe(1);
+    });
+
+    it("fizzBuzz function return 1 when given 1", function() {
+        let main = new Main();
+
+        let result = main.fizzByzz(1);
 
         expect(result).toBe(1);
     });
--- src/app.js
+++ src/app.js
@@ -2,4 +2,8 @@ export class Main {
     demo() {
         return 1;
     }
+
+    fizzByzz(number) {
+        return 1;
+    }
 }
