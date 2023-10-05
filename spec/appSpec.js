af6aed2ac3a0cb8e14e8d70694bd2bc8d4675f29
Sébastien de Beauffort <sebastien.debeauffort@vinci.be>
2023-10-05 17:46:29 +0200

    refactor: externlise the instanciation of the Main class

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -1,23 +1,19 @@
 import { Main } from '../src/app.js';
 
 describe("A suite of test for the demo function of the main class", function() {
+    let main = new Main();
+    
     it("fizzBuzz function should return 1 when given 1", function() {
-        let main = new Main();
-
         let result = main.fizzByzz(1);
 
         expect(result).toBe(1);
     });
     it("fizzBuzz function should return 2 when given 2", function() {
-        let main = new Main();
-        
         let result = main.fizzByzz(2);
 
         expect(result).toBe(2);
     })
     it("fizzbuzz function should return fizz when given 3", function() {
-        let main = new Main();
-        
         let result = main.fizzByzz(3);
 
         expect(result).toBe("fizz");
