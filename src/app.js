9e1a757441e3a12b303866926a175cc35df48fd5
Sébastien de Beauffort <sebastien.debeauffort@vinci.be>
2023-10-12 16:18:43 +0200

    refactor: modulo 3

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 export class Main {
     fizzByzz(number) {
-        if(number === 3 || number ===6) return "fizz";
+        if(number%3 === 0) return "fizz";
         if(number === 5) return "buzz";
 
         return number;
